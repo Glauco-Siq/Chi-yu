055f434947d68646c31d7a2da027039160bf092c
Glauco Siqueira <glauco0909@gmail.com>
2025-10-12 20:12:38 -0300

    inicializando o spring

---
+++ src/main/java/com/pokeCalc/chi_yu/ChiYuApplication.java
@@ -0,0 +1,13 @@
+package com.pokeCalc.chi_yu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiYuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiYuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokeCalc/chi_yu/ChiYuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokeCalc.chi_yu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiYuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
